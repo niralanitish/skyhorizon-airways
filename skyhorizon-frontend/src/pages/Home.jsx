50542ebecf907a9fe1a751d5d1e8dec1615e6faa
Nitish Nirala <nnirala110@gmail.com>
2026-06-22 19:01:12 +0530

    Restructured project into monorepo and added React frontend

---
+++ skyhorizon-frontend/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import Navbar from "../components/Navbar";
+
+function Home() {
+
+    return (
+        <>
+
+            <Navbar />
+
+            <h1>Home Page</h1>
+
+        </>
+    );
+
+}
+
+export default Home;
